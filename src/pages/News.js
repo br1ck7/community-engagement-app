0b3dab00411941d0f14ea418352f17ae5f7fbc8c
Ent-Pendapala <pendashikwambs@gmail.com>
2023-11-01 00:01:21 +0200

    Stuff

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,14 +1,33 @@
 import React from "react";
 import NewsCarousel from "../components/NewsCarousel";
+import { Card, Container, Row, Col } from "react-bootstrap";
 
 function News() {
-	return (
-		<>
-			<div>
-				<NewsCarousel />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div>
+        <NewsCarousel />
+      </div>
+
+      <main>
+        <Container>
+          <Row className="px-4 my-5">
+            <Col>
+              <Card>
+                <Card.Img variant="top" src="https://picsum.photos/160/70" />
+                <Card.Body>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </main>
+    </>
+  );
 }
 
 export default News;
